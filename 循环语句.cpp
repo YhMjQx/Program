a668102957edc9bbd69cfcbc9ef6701b25c4658a
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2022-10-20 22:16:18 +0800

    Add files via upload

---
+++ 循环语句.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main()
+{
+	int line = 0;//定义变量
+	printf("学技术，提能力，合胡杨\n");
+	while (line < 20000)//循环语句
+	{
+		printf("学C语言，提升技术，为了胡杨%d\n",line);//要想把line的值用printf打出来，则别忘了%d，把值给line
+		line++;//line自加
+	}
+	if (line >= 2000)
+		printf("有技术，有能力，胡杨合\n");
+	return 0;
+}

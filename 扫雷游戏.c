7e303a679da3970a93885ac873dcfe10f681886a
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2022-11-23 22:03:21 +0800

    Add files via upload

---
+++ 扫雷游戏.c
@@ -0,0 +1,28 @@
+#define _CRT_SECURE_NO_WARNINGS 1
+#include"É¨À×ÓÎÏ·.h"
+#include<stdio.h>
+void Initboard(char board[ROWS][COLS], int rows, int cols,char set)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 0; i < rows; i++)
+	{
+		for (j = 0; j < cols; j++)
+		{
+			board[i][j] = set;
+		}
+	}
+}
+void Displayboard(char board[ROWS][COLS], int row, int col)
+{
+	int i = 0;
+	int j = 0;
+	for (i = 1; i <= row; i++)
+	{
+		for (j = 1; j <= col; j++)
+		{
+			printf("%c ", board[i][j]);
+		}
+		printf("\n");
+	}
+}

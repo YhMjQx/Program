0b136ed1ffc44e2ed228ff03900322a54b14d610
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2023-04-12 20:29:27 +0800

    Add files via upload

--- Test1.cpp
+++ Test1.cpp
@@ -1,48 +1,85 @@
 #include<iostream>
 using namespace std;
- 
+
+
+class ObjectB{
+public:
+	ObjectB(){
+		cout << "ObjectB..." << endl;
+	}
+	ObjectB(int b) :objb_(b){
+
+	}
+	~ObjectB(){
+		cout << "~ObjectB..." << endl;
+	}
+	int objb_;
+};
+
+class ObjectD{
+public:
+	ObjectD(){
+
+	}
+	ObjectD(int d): objd_(d){
+		cout << "ObjectD..." << endl;
+	}
+	~ObjectD(){
+		cout << "~ObjectD..." << endl;
+	}
+	int objd_;
+};
+
 class Base{
 public:
-	int x_;
-protected:
-	//可以在派生类中访问
-	int y_;
-private:
-	//即使是在派生类中也不能访问
-	int z_;
-};
-
-//公有继承
-class PublicInherit : public Base{
-public:
-	void Test(){
-		x_ = 10;
-		y_ = 20;
-		//z_ = 30;//z_在基类中是private，那么在公有继承中派生类不能直接访问原基类的private成员
+	Base(){
+		cout << "Base..." << endl;
 	}
-private:
-	int a_;
-};
-
-//私有继承
-//基类中的公有和保护成员在派生类中都变成了私有的,相当于x_和y_在派生类中是private:
-class PrivateInherit : private Base{
-public:
-	void Test(){
-		x_ = 10;
-		y_ = 20;
-		//z_ = 30;// - error
+	Base(int b) :b_(b){
+		cout << "Base..." << endl;
 	}
-protected:
+	//拷贝构造函数，将该类的成员们都在初始化列表中进行初始化是较好的
+	Base(const Base& other) :objb_(other.objb_), b_(other.b_)
+	{
 
-private:
-
+	}
+	~Base(){
+		cout << "~Base..." << endl;
+	}
+	int b_;
+	ObjectB objb_;
 };
 
-//保护继承
-//基类中的public数据成员在派生类中是protected成员，而基类中的protected数据成员在派生类中性质也是protected
-//基类中的private数据成员在派生类中是不可直接访问的
+class Derived :public Base{
+public:
+	Derived(int b,int d) :d_(d), Base(b),objd_(222)
+	{
+		cout << "Derived..." << endl;
+	}
+
+	//拷贝构造函数和普通构造函数的初始化列表应该是差不多的，例如参数个数，参数类型
+	Derived(const Derived& other) :d_(other.d_), objd_(other.objd_), Base(other)
+	{
+		
+	}
+	~Derived(){
+		cout << "~Derived..." << endl;
+	}
+	int d_;
+	ObjectD objd_;
+};
+
 int main(){
+	Derived d1(39,93);
+	//不管如何，都会先构造Base类的对象，然后在构造派生类对象
+	cout << "Base - " << d1.b_ <<endl<< "Derived - " << d1.d_ << endl;
 
+	Base b1(100);
+	Base b2(b1);
+	cout << b2.b_ << endl;
+
+
+	//Derived d2(d1);
 	return 0;
 }
+//不管新类是如何调用旧类的对象成员，如果旧类没有默认构造函数，那么就必须在新类的构造函数的初始化列表中对其进行初始化

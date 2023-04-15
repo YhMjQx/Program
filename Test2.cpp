b2268378f37f8e42af250e90135675f6448768c7
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2023-04-15 09:28:32 +0800

    Add files via upload

--- Test2.cpp
+++ Test2.cpp
@@ -1,55 +1,60 @@
 #include<iostream>
 using namespace std;
 
-class Base{
+class Furniture{
 public:
-	Base() :x_(0){
-	}
+	Furniture(int weight) :weight_(weight){
 
-	int GetBaseX() const{
-		return x_;
 	}
+	int weight_;
+	
 
-	void Show(){
-		cout << "Base::Show..." << endl;
+};
+class Bed:public Furniture{
+public:
+	Bed(int weight) :Furniture(weight){
+
 	}
-
-	int x_;
+	void Sleep(){
+		cout << "Sleep..." << endl;
+	}
 };
 
-//派生类，这里的x_和基类的x_不一样
-//派生类的x_与基类的x_重定义的时候，如果用派生类来访问数据成员，那么会访问到派生类的数据成员
-class Derived : public Base{
+class Sofa: public Furniture{
 public:
-	Derived() :x_(0){
+	Sofa(int weight) :Furniture(weight){
 
 	}
+	void WatchTV(){
+		cout << "WatchTV..." << endl;
+	}
+};
 
-	int GetDerivedX() const{
-		return x_;
+class SofaBed :public Bed, public Sofa{
+public:
+	SofaBed(int weight) : Bed(weight), Sofa(weight){
+		FoldIn();
 	}
 
-	void Show(int n){
-		cout << "Derived::Show..." << endl;
+	void FoldOut(){
+		cout << "FoldOut..." << endl;
 	}
 
-	void Show(){
-		cout << "Derived::Show..." << endl;
-
+	void FoldIn(){
+		cout << "FoldInt..." << endl;
 	}
-	int x_;
+
+
 };
 
 int main(){
+	SofaBed sb(100);
+	//sb.weight_ = 39;
+	//sb.weight_ = 93;
+	//以上的指定是不明确的，应该显示指定
 
-	Derived d;
-	d.x_ = 39;
-	d.Base::x_ = 93;//想要访问基类当中的数据成员就得这么写
-	cout << d.GetBaseX() << endl;
-	cout << d.GetDerivedX() << endl;
+	sb.Bed::weight_ = 93;
+	sb.Sofa::weight_ = 39;
 
-	d.Show();
-	d.Show(0);
-	d.Base::Show();//想要访问基类中的成员函数就要这么写
 	return 0;
 }

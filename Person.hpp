43fcf3b41c4815642a2d1848da5d9a70aea15f79
杨明强 <115909869+YhMjQx@users.noreply.github.com>
2023-05-06 18:33:00 +0800

    Add files via upload

---
+++ Person.hpp
@@ -0,0 +1,25 @@
+#include<iostream>
+#include<string>
+using namespace std;
+
+template<class T1, class T2>
+class Person{
+public:
+	Person(T1 name, T2 age);
+
+	void ShowPerson();
+
+	T1 m_Name;
+	T2 m_Age;
+};
+
+template<class T1, class T2>
+Person<T1, T2>::Person(T1 name, T2 age){
+	this->m_Name = name;
+	this->m_Age = age;
+}
+
+template<class T1, class T2>
+void Person<T1, T2>::ShowPerson(){
+	cout << "ÐÕÃû£º " << this->m_Name << " ÄêÁä£º " << this->m_Age << endl;
+}
